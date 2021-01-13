e40c2f58a5512469791aea04ddc3143b4f67bba9
unknown <solar3070@naver.com>
2021-01-14 08:29:10 +0900

    ball collisions and bouncing with js

--- block.js
+++ block.js
@@ -1,36 +1,65 @@
-export class Block {
-    constructor(width, height, x, y) {
-        this.width = width;
-        this.height = height;
-        this.x = x;
-        this.y = y;
-        this.maxX = width + x;
-        this.maxy = height + y;
+export class Ball {
+    constructor(stageWidth, stageHeight, radius, speed) {
+        this.radius = radius;
+        this.vx = speed;
+        this.vy = speed;
+
+        const diameter = this.radius * 2;
+        this.x = this.radius + (Math.random() * stageWidth - diameter);
+        this.y = this.radius + (Math.random() * stageHeight - diameter);
     }
 
-    draw(ctx) {
-        const xGap = 80;
-        const yGap = 60;
+    draw(ctx, stageWidth, stageHeight, block) {
+        this.x += this.vx;
+        this.y += this.vy;
 
-        ctx.fillStyle = '#ff384e';
-        ctx.beginPath();
-        ctx.rect(this.x, this.y, this.width, this.height);
-        ctx.fill();
+        this.bounceWindow(stageWidth, stageHeight);
 
-        ctx.fillStyle = '#190f3a';
-        ctx.beginPath();
-        ctx.moveTo(this.maxX, this.maxY);
-        ctx.lineTo(this.maxX - xGap, this.maxY + yGap);
-        ctx.lineTo(this.x - xGap, this.maxY + yGap);
-        ctx.lineTo(this.x, this.maxY);
-        ctx.fill();
+        this.bounceBlock(block);
 
-        ctx.fillStyle = '#9d0919';
+        ctx.fillStyle = '#fdd700';
         ctx.beginPath();
-        ctx.moveTo(this.x, this.y);
-        ctx.lineTo(this.x, this.maxY);
-        ctx.lineTo(this.x - xGap, this.maxY + yGap);
-        ctx.lineTo(this.x - xGap, this.maxY + yGap - this.height);
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         ctx.fill();
+    }
+
+    bounceWindow(stageWidth, stageHeight) {
+        const minX = this.radius;
+        const maxX = stageWidth - this.radius;
+        const minY = this.radius;
+        const maxY = stageHeight - this.radius;
+
+        if (this.x <= minX || this.x >= maxX) {
+            this.vx *= -1;
+            this.x += this.vx;
+        } else if (this.y <= minY || this.y >= maxY) {
+            this.vy *= -1;
+            this.y += this.vy;
+        }
+    }
+
+    bounceBlock(block) {
+        const minX = block.x - this.radius;
+        const maxX = block.maxX + this.radius;
+        const minY = block.y - this.radius;
+        const maxY = block.maxY + this.radius;
+
+        if (this.x > minX && this.x < maxX && this.y > minY && this.y < maxY) {
+            const x1 = Math.abs(minX - this.x);
+            const x2 = Math.abs(this.x - maxX);
+            const y1 = Math.abs(minY - this.y);
+            const y2 = Math.abs(this.y - maxY);
+            const min1 = Math.min(x1, x2);
+            const min2 = Math.min(y1, y2);
+            const min = Math.min(min1, min2);
+
+            if (min == min1) {
+                this.vx *= -1;
+                this.x += this.vx;
+            } else if (min == min2) {
+                this.vy *= -1;
+                this.y += this.vy;
+            }
+        }
     }
 }
